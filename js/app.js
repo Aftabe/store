f070b64de8d9c58d3507e76dbb98e63f252289a1
Aftabe <shamsasghari77@gmail.com>
2020-08-28 17:50:26 +1000

    code modifeid

--- js/app.js
+++ js/app.js
@@ -1,27 +1,27 @@
-(function(){
-    let day;
-switch (new Date().getDay()) {
-  case 0:
-    day = "Sunday";
-    break;
-  case 1:
-    day = "Monday";
-    break;
-  case 2:
-    day = "Tuesday";
-    break;
-  case 3:
-    day = "Wednesday";
-    break;
-  case 4:
-    day = "Thursday";
-    break;
-  case 5:
-    day = "Friday";
-    break;
-  case  6:
-    day = "Saturday";
-}
-document.getElementById("demo").innerHTML = "Today is " + day;
-alert('today is ' + day);
+(function () {
+  let day;
+  switch (new Date().getDay()) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+  }
+  document.getElementById("demo").innerHTML = "Today is " + day;
+  alert('today is ' + day);
 }());
